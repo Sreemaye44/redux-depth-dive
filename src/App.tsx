7ffac4cb01b8b2b129fa763f36fac80dc8d5d543
sreemaye <sreemaye44@gmail.com>
2024-01-10 16:35:02 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="h-screen w-full flex justify-center items-center">
+        <div className="flex border border-purple-300 rounded-md bg-slate-50 p-10">
+          <button className="px-3 py-2 rounded-md bg-green-300 text-xl font-semibold">Increment</button>
+          <h1 className="text-3xl mx-10">0</h1>
+          <button className="px-3 py-2 rounded-md bg-red-300 text-xl font-semibold">Decrement</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
