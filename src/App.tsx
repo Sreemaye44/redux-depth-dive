b3243ad8fb43eb62d2e74e5cf79a094175e95f0d
sreemaye <sreemaye44@gmail.com>
2024-01-10 17:53:25 +0600

    reducer created

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
+import { useDispatch, useSelector } from "react-redux"
+import { decrement, increment, incrementByValue } from "./redux/features/counterSlice";
 
 
 function App() {
+
+  const { count } = useSelector((state) => state.counter)
+
+  const dispatch = useDispatch();
 
 
   return (
     <>
       <div className="h-screen w-full flex justify-center items-center">
         <div className="flex border border-purple-300 rounded-md bg-slate-50 p-10">
-          <button className="px-3 py-2 rounded-md bg-green-300 text-xl font-semibold">Increment</button>
-          <h1 className="text-3xl mx-10">0</h1>
-          <button className="px-3 py-2 rounded-md bg-red-300 text-xl font-semibold">Decrement</button>
+          <button onClick={() => dispatch(increment())} className="px-3 py-2 rounded-md bg-green-300 text-xl font-semibold">Increment</button>
+          <button onClick={() => dispatch(incrementByValue(5))} className="px-3 py-2 rounded-md bg-green-300 text-xl font-semibold">Increment by 5</button>
+          <h1 className="text-3xl mx-10">{count}</h1>
+          <button onClick={() => dispatch(decrement())} className="px-3 py-2 rounded-md bg-red-300 text-xl font-semibold">Decrement</button>
         </div>
       </div>
     </>
